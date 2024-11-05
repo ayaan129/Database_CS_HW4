c5a2063ad0db7e5b3c00673ecc90efaf4c269c5b
Rinzy <atermizor@gmail.com>
2024-11-05 15:49:00 -0600

    Update delete_data.sql

--- sql/delete_data.sql
+++ sql/delete_data.sql
@@ -3,10 +3,6 @@
 -- Start a transaction to ensure all deletions are atomic
 BEGIN;
 
--- Disable foreign key constraints temporarily (optional, if needed)
--- This step is not strictly necessary when using TRUNCATE ... CASCADE
--- SET session_replication_role = 'replica';
-
 -- Truncate all tables to remove all data
 TRUNCATE TABLE payment CASCADE;
 TRUNCATE TABLE bill CASCADE;
@@ -15,16 +11,8 @@ TRUNCATE TABLE customer CASCADE;
 TRUNCATE TABLE bank_account CASCADE;
 TRUNCATE TABLE phone_plan CASCADE;
 
--- Alternatively, if you prefer to use DELETE statements in order:
--- DELETE FROM payment;
--- DELETE FROM bill;
--- DELETE FROM call_record;
--- DELETE FROM customer;
--- DELETE FROM bank_account;
--- DELETE FROM phone_plan;
+
 
 -- Commit the transaction
 COMMIT;
 
--- Re-enable foreign key constraints if they were disabled
--- SET session_replication_role = 'origin';
